a147956f7cb3fb5b36aae6306331e742f0d5cc05
JumpyJacko <bobjacklee27@gmail.com>
2022-07-31 18:50:15 +1000

    Removed right-side GUI mod-tap

--- keyboards/ferris/keymaps/JumpyJacko/keymap.c
+++ keyboards/ferris/keymaps/JumpyJacko/keymap.c
@@ -18,7 +18,6 @@
 #define HOME_N LCTL_T(KC_N)
 #define HOME_E LSFT_T(KC_E)
 #define HOME_I LALT_T(KC_I)
-#define HOME_O LGUI_T(KC_O)
 
 /* Defining Homerow Mods for Readability (QWERTY) */
 #define HOME_QA    LGUI_T(KC_A)
@@ -29,7 +28,6 @@
 #define HOME_QJ    LCTL_T(KC_J)
 #define HOME_QK    LSFT_T(KC_K)
 #define HOME_QL    LALT_T(KC_L)
-#define HOME_QSCLN LGUI_T(KC_SCLN)
 
 /* Defining Layers - Now in artsey.h*/
 
@@ -47,8 +45,8 @@
 #include "artsey.c"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_COLEMAK] = LAYOUT(     KC_Q,   KC_W,   KC_F,   KC_P,    KC_G,   KC_J,  KC_L,   KC_U,     KC_Y,    KC_SCLN, 
-                           HOME_A, HOME_R, HOME_S, HOME_T,  KC_D,   KC_H,  HOME_N, HOME_E,   HOME_I,  HOME_O, 
+    [_COLEMAK] = LAYOUT(   KC_Q,   KC_W,   KC_F,   KC_P,    KC_G,   KC_J,  KC_L,   KC_U,     KC_Y,    KC_SCLN, 
+                           HOME_A, HOME_R, HOME_S, HOME_T,  KC_D,   KC_H,  HOME_N, HOME_E,   HOME_I,  KC_O, 
                            KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,   KC_K,  KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, 
                                                    KC_BSPC, NUMS,   NAVI,  KC_SPC
         ),
@@ -63,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         *                   | ent |bkspc|        | spc | tab |
                         *                   '-----------'        '-----------'
                         */
-    [_QWERTY] = LAYOUT(     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,      KC_O,     KC_P, 
-                           HOME_QA, HOME_QS, HOME_QD, HOME_QF, KC_G,   KC_H,   HOME_QJ, HOME_QK,   HOME_QL,  HOME_QSCLN, 
+    [_QWERTY] = LAYOUT(    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,      KC_O,     KC_P, 
+                           HOME_QA, HOME_QS, HOME_QD, HOME_QF, KC_G,   KC_H,   HOME_QJ, HOME_QK,   HOME_QL,  KC_SCLN, 
                            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM,   KC_DOT,   KC_SLSH, 
                                                       KC_BSPC, NUMS,   NAVI,   KC_SPC
         ),
